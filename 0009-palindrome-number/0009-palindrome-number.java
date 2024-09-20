1280ee6003d306c87edf08fc9b02a562977c56df
princeyadav21 <prince981137@gmail.com>
2024-09-20 13:20:20 +0530

    Time: 4 ms (100.00%), Space: 44.1 MB (26.65%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) 
+    {
+        int temp=x;
+        int sum=0;
+        while(temp>0)
+        {
+            int rem=temp%10;
+            sum=sum*10+rem;
+            temp=temp/10;
+        }
+        if(sum==x)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
